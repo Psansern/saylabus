dbf0a9b8e372e30a454745d414ed7e26279562c8
sansen phawandee <friday@sansens-MacBook-Air.local>
2020-10-02 09:45:57 +0700

    create migration college and position

---
+++ database/migrations/2020_10_02_084927_create_position_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePositionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('position', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('position_name');
+            $table->string('position_short_name');
+            $table->string('position_name_eng');
+            $table->string('position_short_name_eng');
+            $table->string('user_edit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('position');
+    }
+}
---
+++ database/migrations/2020_10_02_085134_create_edu_blackground_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEduBlackgroundTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('edu_blackground', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('edu_blackground');
+    }
+}
