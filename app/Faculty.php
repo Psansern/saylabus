d2b038c4d6ac0df31cf11c1437ac1941d2b44dfd
sansern <sansern.par@gmail.com>
2020-10-12 15:52:14 +0700

    update model faculty

--- app/Faculty.php
+++ app/Faculty.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Faculty extends Model
 {
     protected $table='faculty'; // table names
-    protected $fillable=['faculty_id','faculty_name','faculty_name_eng','user_edit'];
+    protected $fillable=['faculty_id',
+                        'faculty_code',
+                        'ref_fac_id',
+                        'faculty_name',
+                        'faculty_name_eng',
+                        'user_edit'];
 
 }
