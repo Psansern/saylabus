4c2e88aa04aa8bb1692d31559ee620dd3693e430
sansern <sansern.par@gmail.com>
2020-10-12 15:51:09 +0700

    update index faculty

--- resources/views/faculty/index.blade.php
+++ resources/views/faculty/index.blade.php
@@ -1,3 +1,40 @@
-@extends('main.layout')
+@extends('layouts.app')
 
-<h1> SAYLABUS </h1>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">รายชื่อคณะและศูนย์สำนัก</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+<!-- เขียนส่วนแสดงผล-->
+
+<table class="table table-bordered">
+    <tr>
+        <th><center>ลำดับ</center></th>
+        <th><center>รายชื่อคณะ </center></th>
+
+        </tr>
+        @foreach ($faculty as $fac)
+        <tr>
+            <td><center>{{$fac->faculty_id}}</center></td>
+            <td><a href="">{{$fac->faculty_name}}</a></td>
+        </tr>
+        @endforeach
+</table>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
